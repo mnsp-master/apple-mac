7df4e752bc264f717deb9a96e56e33a347127405
mnsp-snoble <snoadmin@msnpartnership.com>
2023-02-06 15:25:52 +0000

    Update mnsp-login-common.sh

--- mnsp-login-common.sh
+++ mnsp-login-common.sh
@@ -2,7 +2,7 @@
 # *********************************************************************
 
 # Script Configuration
-CNF_VER="0.9.8.9.3" #script version used for update checking
+CNF_VER="0.9.8.9.4" #script version used for update checking
 CNF_ENABLED="YES" #run script yes or no
 CNF_LOGGING="YES" #log script output or not
 CNF_UPDATES="YES" #check mac server for updates and download them
@@ -102,6 +102,7 @@ fi
 	shasum -a 256 -c "$CNF_SETUP/.scripts/mnsp-login-common.checksum" -q #compare checksums
 		if [ $? -ne 0 ] ; then #if checksums do not match
 		_mainLog "inf" "Downloading latest script..."
+		#curl --url $CNF_GITSRC --output "$CNF_SETUP/.scripts/mnsp-login-common.sh" > /dev/null
 		curl --url $CNF_GITSRC --output "$CNF_SETUP/.scripts/mnsp-login-common.sh" > /dev/null
 		RES=$?
 		if [ "$RES" != "0"] ; then
