1c6989aa3e27c99158f02eeb1395ae4631990428
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-02-24 11:57:14 +0000

    int year investigatio

--- mnsp-login-common.sh
+++ mnsp-login-common.sh
@@ -2,7 +2,7 @@
 # *********************************************************************
 
 # Script Configuration
-CNF_VER="0.9.8.9.7" #script version used for update checking
+CNF_VER="0.9.8.9.8" #script version used for update checking
 CNF_ENABLED="YES" #run script yes or no
 CNF_LOGGING="YES" #log script output or not
 CNF_UPDATES="YES" #check mac server for updates and download them
@@ -174,7 +174,7 @@ if [[ "${VAR_ROLE}" =~ "Students" ]] ;then
 		#iterate each element in array
 		for element in "${array[@]}"
 		do
-		#echo $element
+		echo $element
 		#find array element containing numerical value, and set INTYR var accordingly
 			case $element in
 				''|*[!0-9]*) ;;
