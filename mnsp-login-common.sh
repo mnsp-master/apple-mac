cf183e4bdc862a76bc1db30f6419150d2c7af7d3
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-02-24 22:35:21 +0000

    intake year case

--- mnsp-login-common.sh
+++ mnsp-login-common.sh
@@ -2,7 +2,7 @@
 # *********************************************************************
 #osver at bcl 12.5
 # Script Configuration
-CNF_VER="0.9.8.9.9.3" #script version used for update checking
+CNF_VER="0.9.8.9.9.4" #script version used for update checking
 CNF_ENABLED="YES" #run script yes or no
 CNF_LOGGING="YES" #log script output or not
 CNF_UPDATES="YES" #check mac server for updates and download them
